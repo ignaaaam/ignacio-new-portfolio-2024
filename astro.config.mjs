bebddc78888c3c12ff38963c7759b36b47426e69
Ignacio <ignasiamat10@gmail.com>
2024-02-19 01:50:15 +0100

    astro config web analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [
+    tailwind()
+  ],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
