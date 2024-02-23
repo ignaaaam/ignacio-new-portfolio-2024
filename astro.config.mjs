1f40bac413dfd43be33edcb8589fd811fae3e2eb
Ignacio <ignasiamat10@gmail.com>
2024-02-23 07:51:31 +0100

    React added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
-import partytown from '@astrojs/partytown';
+import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
+import vercelStatic from "@astrojs/vercel/static";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: "static",
   integrations: [
     tailwind(),
     partytown({
       config: {
-        config: { debug: false },
-        forward: ['dataLayer.push'],
+        config: {
+          debug: false,
+        },
+        forward: ["dataLayer.push"],
       },
     }),
     sitemap(),
+    react(),
   ],
   adapter: vercelStatic({
     webAnalytics: {
-      enabled: true
+      enabled: true,
     },
     speedInsights: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
 });
