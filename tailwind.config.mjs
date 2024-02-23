9481b5b93affd5c970aec07a857da5808c5636e7
Ignacio <ignasiamat10@gmail.com>
2024-02-23 07:51:55 +0100

    Old react component removed, new one added but commented at the moment for testing purposes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,22 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  animation: {
+			"meteor-effect": "meteor 5s linear infinite",
+		  },
+		  keyframes: {
+	
+			meteor: {
+			  "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+			  "70%": { opacity: 1 },
+			  "100%": {
+				transform: "rotate(215deg) translateX(-500px)",
+				opacity: 0,
+			  },
+			},
+		  },
+		},
+	  },
 	plugins: [],
 }
