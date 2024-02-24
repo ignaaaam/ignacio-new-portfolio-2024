335fa0809dc42a4952315150b34bb44504db9419
Ignacio <ignasiamat10@gmail.com>
2024-02-24 18:44:27 +0100

    flowbite & meteor effect added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-		  animation: {
-			"meteor-effect": "meteor 5s linear infinite",
-		  },
-		  keyframes: {
-	
-			meteor: {
-			  "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-			  "70%": { opacity: 1 },
-			  "100%": {
-				transform: "rotate(215deg) translateX(-500px)",
-				opacity: 0,
-			  },
-			},
-		  },
-		},
-	  },
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+          "70%": { opacity: 1 },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: 0,
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
