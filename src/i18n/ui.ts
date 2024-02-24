41b787203be03ad36d8b271bb40f52c7300380a6
Ignacio <ignasiamat10@gmail.com>
2024-02-24 18:44:43 +0100

    i18n added

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+    en: 'English',
+    es: 'Spanish',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    en: {
+      'keywords': 'Full Stack Web Developer, Back End Developer, Front End Developer, Web Development, Web Design, Laravel, Livewire, Inertia, Vue 3, SEO, Innovation, Quality, Mid-Level Developer, Junior Developer, Freelance Developer, Spain, Barcelona, Madrid, Valencia, Worldwide',
+    },
+    es: {
+      'keywords': 'Desarrollador Web Full Stack, Desarrollador Back End, Desarrollador Front End, Desarrollo Web, Diseño Web, Laravel, Livewire, Inertia, Vue 3, SEO, Innovación, Calidad, Desarrollador Nivel Medio, Desarrollador Junior, Desarrollador Freelance, España, Barcelona, Madrid, Valencia, Mundial',
+    },
+  } as const;
