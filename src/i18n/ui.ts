6339991c70f8cc007c1e59378ee1d53e70431718
Ignacio <ignasiamat10@gmail.com>
2024-03-16 20:26:27 +0100

    404 error website added

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,8 +8,14 @@ export const languages = {
   export const ui = {
     en: {
       'keywords': 'Full Stack Web Developer, Back End Developer, Front End Developer, Web Development, Web Design, Laravel, Livewire, Inertia, Vue 3, SEO, Innovation, Quality, Mid-Level Developer, Junior Developer, Freelance Developer, Spain, Barcelona, Madrid, Valencia, Worldwide',
+      '404-title': 'Page not found',
+      '404-content': 'The page you are looking for does not exist.',
+      '404-button': 'Go back to home page',
     },
     es: {
       'keywords': 'Desarrollador Web Full Stack, Desarrollador Back End, Desarrollador Front End, Desarrollo Web, Diseño Web, Laravel, Livewire, Inertia, Vue 3, SEO, Innovación, Calidad, Desarrollador Nivel Medio, Desarrollador Junior, Desarrollador Freelance, España, Barcelona, Madrid, Valencia, Mundial',
+      '404-title': 'Página no encontrada',
+      '404-content': 'Lo siento, la página que estás buscando no se pudo encontrar.',
+      '404-button': 'Volver a la página de inicio',
     },
   } as const;
