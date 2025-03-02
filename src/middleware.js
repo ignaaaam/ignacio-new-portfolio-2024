332244bc0d95d0a264c12bdda9d363d1271f5818
ignaaaam <ignasiamat10@gmail.com>
2025-03-02 16:47:06 +0100

    Download button updated, new CV added, SocialPills container and index (also Layout) adjusted for mobile sizes too.

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ const downloadTracker = new Map();
 // Set download limits
 const DOWNLOAD_LIMIT = 4;         // Downloads allowed
 const TIME_WINDOW = 60 * 60 * 1000; // 1 hour in milliseconds
-const CV_PATH = '/CV-UPDATED-2024.pdf';
+const CV_PATH = '/CV-UPDATED-2025.pdf';
 
 export const onRequest = defineMiddleware((context, next) => {
   const { pathname } = new URL(context.request.url);
